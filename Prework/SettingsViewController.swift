7d58f4b687b9fc76e6f33f4c923eb6471b527905
Jasmine Lee <jjl10@rice.edu>
2022-01-02 22:29:16 -0500

    committed all files added to prework assignment

---
+++ Prework/SettingsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsViewController.swift
+//  Prework
+//
+//  Created by Jasmine Lee on 12/19/21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //sets the title in the nav bar
+        self.title = "Tip Calculator"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+//
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        print("view will appear")
+//        // This is a good place to retrieve the default tip percentage from UserDefaults
+//        // and use it to update the tip amount
+//        let tipPercentages = [0.15, 0.18, 0.2]
+//    }
+//
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//        print("view did appear")
+//    }
+//
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//        print("view will disappear")
+//    }
+//
+//    override func viewDidDisappear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//        print("view did disappear")
+//    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        //get bill amt from text field input and if none, let default be 0
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        //get total tip by multiplying tip * tipPercentage
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = tip + bill
+        
+        //update tip amount label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        //update total amount
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
